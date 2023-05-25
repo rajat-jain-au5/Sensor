cfcf2e66078f581acaf90b65e7849d559261bab5
rajat <rajat@edsanta.com>
2023-05-25 15:40:41 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='sensor_fault',
+    version='0.0.1',
+    author='Rajat',
+    author_email='rajatjain852@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
